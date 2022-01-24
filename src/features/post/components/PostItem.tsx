dd7ceabb73a6ddd8968cdee9ff5da2e978db603c
= <=>
2022-01-25 00:50:16 +0200

    add bold effect for found search term

--- src/features/post/components/PostItem.tsx
+++ src/features/post/components/PostItem.tsx
@@ -1,13 +1,32 @@
+import { useAppSelector } from '@src/app/hooks'
 import { IPost } from '@src/type'
 import { FC } from 'react'
 import styles from './PostItem.module.css'
+import match from 'autosuggest-highlight/match'
+import parse from 'autosuggest-highlight/parse'
 const PostItem: FC<{ post: IPost }> = ({ post }) => {
+  const postFilterTerm = useAppSelector((state) => state.filter.postFilterTerm)
+  const matches = match(
+    post.message.toLocaleLowerCase(),
+    postFilterTerm.toLocaleLowerCase()
+  )
+  const parts = parse(post.message, matches)
   return (
     <div className={styles.container}>
       <div className={styles.time}>
         {new Date(post.created_time).toUTCString().slice(0, -3)}
       </div>
-      <div className={styles.content}> {post.message}</div>
+      <div className={styles.content}>
+        {parts.map((part, index) => (
+          <span
+            key={index}
+            style={{
+              fontWeight: part.highlight ? 700 : 400
+            }}>
+            {part.text}
+          </span>
+        ))}
+      </div>
     </div>
   )
 }
--- src/features/post/components/UserItem.tsx
+++ src/features/post/components/UserItem.tsx
@@ -1,4 +1,7 @@
+import { useAppSelector } from '@src/app/hooks'
 import { IUser } from '@src/type'
+import match from 'autosuggest-highlight/match'
+import parse from 'autosuggest-highlight/parse'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { FC } from 'react'
@@ -6,13 +9,29 @@ import styles from './UserItem.module.css'
 const UserItem: FC<{ user: IUser }> = ({ user }) => {
   const router = useRouter()
   const selectedUserId = router.query.user_id
+  const userFilterTerm = useAppSelector((state) => state.filter.userFilterTerm)
+  const matches = match(
+    user.name.toLocaleLowerCase(),
+    userFilterTerm.toLocaleLowerCase()
+  )
+  const parts = parse(user.name, matches)
   return (
     <Link href={`/?user_id=${user.id}`}>
       <div
         className={`${styles.container} ${
           selectedUserId === user.id ? styles.active : ''
         }`}>
-        <div>{user.name}</div>
+        <div>
+          {parts.map((part, index) => (
+            <span
+              key={index}
+              style={{
+                fontWeight: part.highlight ? 700 : 400
+              }}>
+              {part.text}
+            </span>
+          ))}
+        </div>
         <div className={styles.postCount}>{user.postCount}</div>
       </div>
     </Link>
